6628074818ceb42592bba9442693163cfc38d19f
Ozan HACIBEKİROĞLU <ozanhaci@gmail.com>
2022-04-11 04:42:22 +0300

    feat: create mkcallable to generate function helpers

---
+++ futils/futils.go
@@ -0,0 +1,60 @@
+package futils
+
+//go:generate go run ../cmd/mkcallable -output zfutils.go futils.go
+
+// builtin delete
+//
+//ugo:callable func(o ugo.Object, k string) (err error)
+
+// builtin copy, len, error, typeName, bool, string, isInt, isUint
+// isFloat, isChar, isBool, isString, isBytes, isMap, isSyncMap, isArray
+// isUndefined, isFunction, isCallable, isIterable
+// time module IsTime
+//
+//ugo:callable func(o ugo.Object) (ret ugo.Object)
+
+// builtin repeat
+//
+//ugo:callable func(o ugo.Object, n int) (ret ugo.Object, err error)
+
+// builtin contains
+//
+//ugo:callable func(o ugo.Object, v ugo.Object) (ret ugo.Object, err error)
+
+// builtin sort, sortReverse, int, uint, float, char, chars
+// json Marshal
+//
+//ugo:callable func(o ugo.Object) (ret ugo.Object, err error)
+
+// time module MountString, WeekdayString
+//
+//ugo:callable func(i1 int) (ret ugo.Object, err error)
+
+// time module DurationString, DurationHours, DurationMinutes, DurationSeconds
+// DurationMilliseconds, DurationMicroseconds, DurationNanoseconds, Sleep
+//
+//ugo:callable func(i1 int64) (ret ugo.Object, err error)
+
+// time module ParseDuration, LoadLocation
+//
+//ugo:callable func(s string) (ret ugo.Object, err error)
+
+// time module FixedZone
+//
+//ugo:callable func(s string, i1 int64) (ret ugo.Object, err error)
+
+// time module Time, Now
+//
+//ugo:callable func() (ret ugo.Object)
+
+// time module DurationRound, DurationTruncate
+//
+//ugo:callable func(i1 int64, i2 int64) (ret ugo.Object)
+
+// json module Unmarshal
+//
+//ugo:callable func(b []byte) (ret ugo.Object, err error)
+
+// json module MarshalIndent
+//
+//ugo:callable func(o ugo.Object, s1 string, s2 string) (ret ugo.Object, err error)
