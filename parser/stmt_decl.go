7c1fb2bd3d44cbd8b6032e223f098230074051c1
Ozan HACIBEKİROĞLU <ozanhaci@gmail.com>
2022-04-14 10:44:40 +0300

    chore: update copyright year

--- parser/stmt_decl.go
+++ parser/stmt_decl.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE.golang file.
 
-// Copyright (c) 2020 Ozan Hacıbekiroğlu.
+// Copyright (c) 2020-2022 Ozan Hacıbekiroğlu.
 // Use of this source code is governed by a MIT License
 // that can be found in the LICENSE file.
 
